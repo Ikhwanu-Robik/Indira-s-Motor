bfdd9ed59e5eb8df81dda02fb33aaf02a41b39ff
RasyidAf <rasyidamrulfathony@gmail.com>
2025-03-07 10:19:39 +0700

    create project

---
+++ indira_s_motor/src/main/java/com/mycompany/indira_s_motor/Indira_s_motor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.indira_s_motor;
+
+/**
+ *
+ * @author rasyid
+ */
+public class Indira_s_motor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
