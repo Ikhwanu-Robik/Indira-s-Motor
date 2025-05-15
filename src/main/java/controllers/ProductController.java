5cf5ef385352b508e8bb20a2bf3c8c8526507924
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-05-15 22:16:08 +0700

    refactor: make get products more efficient

--- src/main/java/controllers/ProductController.java
+++ src/main/java/controllers/ProductController.java
@@ -19,157 +19,201 @@ import java.util.logging.Logger;
  */
 public class ProductController extends AbstractController {
 
-    public String table_name = "products";
+	public String table_name = "products";
 
-    @Override
-    public ArrayList<HashMap<String, String>> read(ArrayList<String> columns) {
-        Database db = new Database();
-        ResultSet rs;
-        String stringColumns = "";
-        ArrayList<HashMap<String, String>> products = new ArrayList<>();
-        for (String column : columns) {
-            stringColumns += (column + ",");
-        }
-        stringColumns = stringColumns.substring(0, stringColumns.length() - 1);
+	@Override
+	public ArrayList<HashMap<String, String>> read(ArrayList<String> columns) {
+		Database db = new Database();
+		ResultSet rs;
+		String stringColumns = "";
+		ArrayList<HashMap<String, String>> products = new ArrayList<>();
+		for (String column : columns) {
+			stringColumns += (column + ",");
+		}
+		stringColumns = stringColumns.substring(0, stringColumns.length() - 1);
 
-        //edge case where the frontend puts in *
-        if (stringColumns.equals("*")) {
-            stringColumns = "id, name, image_url, price, stock, brand_id";
-            
-            columns.clear();
-            
-            columns.add("id");
-            columns.add("name");
-            columns.add("image_url");
-            columns.add("price");
-            columns.add("stock");
-            columns.add("brand_id");
-        }
+		// edge case where the frontend puts in *
+		if (stringColumns.equals("*")) {
+			stringColumns = "id, name, image_url, price, stock, brand_id";
 
-        try {
-            Statement stmt = db.connect().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            rs = stmt.executeQuery("SELECT " + stringColumns + " FROM " + this.table_name);
+			columns.clear();
 
-            if (!rs.isBeforeFirst()) {
-                db.close();
-                return null;
-            }
+			columns.add("id");
+			columns.add("name");
+			columns.add("image_url");
+			columns.add("price");
+			columns.add("stock");
+			columns.add("brand_id");
+		}
 
-            rs.first();
-            do {
-                HashMap<String, String> row = new HashMap<>();
-                for (String column : columns) {
-                    row.put(column, rs.getString(column));
-                }
-                products.add(row);
-            } while (rs.next());
+		try {
+			Statement stmt = db.connect().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			rs = stmt.executeQuery("SELECT " + stringColumns + " FROM " + this.table_name);
 
-            db.close();
+			if (!rs.isBeforeFirst()) {
+				db.close();
+				return null;
+			}
 
-            return products;
-        } catch (SQLException ex) {
-            Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
-    }
+			rs.first();
+			do {
+				HashMap<String, String> row = new HashMap<>();
+				for (String column : columns) {
+					row.put(column, rs.getString(column));
+				}
+				products.add(row);
+			} while (rs.next());
 
-    @Override
-    public void create(HashMap<String, String> values) {
-        String insertStatement = "INSERT INTO " + this.table_name;
-        insertStatement += " (name, image_url, price, stock, brand_id) VALUES (";
-        insertStatement += "\"" + values.get("name") + "\"" + ", ";
-        insertStatement += "\"" + values.get("image_url") + "\"" + ", ";
-        insertStatement += "\"" + values.get("price") + "\"" + ", ";
-        insertStatement += "\"" + values.get("stock") + "\"" + ", ";
-        insertStatement += "\"" + values.get("brand_id") + "\"";
-        insertStatement += ")";
+			db.close();
 
-        try {
-            Database db = new Database();
-            Statement stmt = db.connect().createStatement();
-            stmt.execute(insertStatement);
+			return products;
+		} catch (SQLException ex) {
+			Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return null;
+	}
 
-            db.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	@Override
+	public void create(HashMap<String, String> values) {
+		String insertStatement = "INSERT INTO " + this.table_name;
+		insertStatement += " (name, image_url, price, stock, brand_id) VALUES (";
+		insertStatement += "\"" + values.get("name") + "\"" + ", ";
+		insertStatement += "\"" + values.get("image_url") + "\"" + ", ";
+		insertStatement += "\"" + values.get("price") + "\"" + ", ";
+		insertStatement += "\"" + values.get("stock") + "\"" + ", ";
+		insertStatement += "\"" + values.get("brand_id") + "\"";
+		insertStatement += ")";
 
-    @Override
-    public void update(int id, HashMap<String, String> values) {
-        String updateStatement = "UPDATE " + this.table_name + " SET";
-        updateStatement += " name = \"" + values.get("name") + "\"" + ", ";
-        updateStatement += " image_url = \"" + values.get("image_url") + "\"" + ", ";
-        updateStatement += " price = \"" + values.get("price") + "\"" + ", ";
-        updateStatement += " stock = \"" + values.get("stock") + "\"" + ", ";
-        updateStatement += " brand_id = \"" + values.get("brand_id") + "\"";
-        updateStatement += " WHERE id = " + id;
-        try {
-            Database db = new Database();
-            Statement stmt = db.connect().createStatement();
-            stmt.execute(updateStatement);
+		try {
+			Database db = new Database();
+			Statement stmt = db.connect().createStatement();
+			stmt.execute(insertStatement);
 
-            db.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+			db.close();
+		} catch (SQLException ex) {
+			Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    @Override
-    public void delete(int id) {
-        String deleteStatement = "DELETE FROM " + this.table_name + " WHERE id = " + id;
+	@Override
+	public void update(int id, HashMap<String, String> values) {
+		String updateStatement = "UPDATE " + this.table_name + " SET";
+		updateStatement += " name = \"" + values.get("name") + "\"" + ", ";
+		updateStatement += " image_url = \"" + values.get("image_url") + "\"" + ", ";
+		updateStatement += " price = \"" + values.get("price") + "\"" + ", ";
+		updateStatement += " stock = \"" + values.get("stock") + "\"" + ", ";
+		updateStatement += " brand_id = \"" + values.get("brand_id") + "\"";
+		updateStatement += " WHERE id = " + id;
+		try {
+			Database db = new Database();
+			Statement stmt = db.connect().createStatement();
+			stmt.execute(updateStatement);
 
-        try {
-            Database db = new Database();
-            Statement stmt = db.connect().createStatement();
-            stmt.execute(deleteStatement);
+			db.close();
+		} catch (SQLException ex) {
+			Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-            db.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+	@Override
+	public void delete(int id) {
+		String deleteStatement = "DELETE FROM " + this.table_name + " WHERE id = " + id;
 
-    }
+		try {
+			Database db = new Database();
+			Statement stmt = db.connect().createStatement();
+			stmt.execute(deleteStatement);
 
-    @Override
-    public ArrayList<HashMap<String, String>> findWhere(String column, String value) {
-        String whereStatement = "SELECT * FROM " + this.table_name + " WHERE " + column + " = \"" + value + "\"";
-        try {
-            Database db = new Database();
-            ResultSet rs;
-            ArrayList<HashMap<String, String>> products = new ArrayList<>();
-            Statement stmt = db.connect().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            rs = stmt.executeQuery(whereStatement);
+			db.close();
+		} catch (SQLException ex) {
+			Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
+		}
 
-            if (!rs.isBeforeFirst()) {
-                db.close();
-                return null;
-            }
-            //I expect the line below will throw an exception if ResultSet is empty a.k.a no result found for such query
-            rs.first();
-            do {
-                HashMap<String, String> row = new HashMap<>();
-                row.put("id", rs.getString("id"));
-                row.put("name", rs.getString("name"));
-                row.put("image_url", rs.getString("image_url"));
-                row.put("price", rs.getString("price"));
-                row.put("stock", rs.getString("stock"));
-                row.put("brand_id", rs.getString("brand_id"));
-                products.add(row);
-            } while (rs.next());
+	}
 
-            db.close();
+	@Override
+	public ArrayList<HashMap<String, String>> findWhere(String column, String value) {
+		String whereStatement = "SELECT * FROM " + this.table_name + " WHERE " + column + " = \"" + value + "\"";
+		try {
+			Database db = new Database();
+			ResultSet rs;
+			ArrayList<HashMap<String, String>> products = new ArrayList<>();
+			Statement stmt = db.connect().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			rs = stmt.executeQuery(whereStatement);
 
-            return products;
-        } catch (SQLException ex) {
-            Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+			if (!rs.isBeforeFirst()) {
+				db.close();
+				return null;
+			}
+			// I expect the line below will throw an exception if ResultSet is empty a.k.a
+			// no result found for such query
+			rs.first();
+			do {
+				HashMap<String, String> row = new HashMap<>();
+				row.put("id", rs.getString("id"));
+				row.put("name", rs.getString("name"));
+				row.put("image_url", rs.getString("image_url"));
+				row.put("price", rs.getString("price"));
+				row.put("stock", rs.getString("stock"));
+				row.put("brand_id", rs.getString("brand_id"));
+				products.add(row);
+			} while (rs.next());
 
-        return null;
-    }
+			db.close();
 
-    public ArrayList<HashMap<String, String>> getBrand(int product_id) {
-        String id = this.findWhere("id", Integer.toString(product_id)).getFirst().get("brand_id");
-        return new BrandController().findWhere("id", id);
-    }
+			return products;
+		} catch (SQLException ex) {
+			Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		return null;
+	}
+
+	public ArrayList<HashMap<String, String>> getBrand(int product_id) {
+		String id = this.findWhere("id", Integer.toString(product_id)).getFirst().get("brand_id");
+		return new BrandController().findWhere("id", id);
+	}
+
+	public ArrayList<HashMap<String, String>> getDetailedProducts() {
+		Database db = new Database();
+		ResultSet rs;
+		String stringColumns = "";
+		ArrayList<HashMap<String, String>> products = new ArrayList<>();
+		ArrayList<String> columns = new ArrayList<>();
+		columns.add("id");
+		columns.add("name");
+		columns.add("image_url");
+		columns.add("price");
+		columns.add("stock");
+		columns.add("brand_id");
+		columns.add("brand_name");
+		columns.add("category_name");
+
+		try {
+			Statement stmt = db.connect().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			rs = stmt.executeQuery("SELECT products.id, products.name, image_url, price, stock, brands.id AS brand_id, brands.name AS brand_name, categories.name AS category_name FROM " + this.table_name + " JOIN brands ON products.brand_id = brands.id JOIN categories ON brands.category_id = categories.id");
+
+			if (!rs.isBeforeFirst()) {
+				db.close();
+				return null;
+			}
+
+			rs.first();
+			do {
+				HashMap<String, String> row = new HashMap<>();
+				for (String column : columns) {
+					row.put(column, rs.getString(column));
+				}
+				products.add(row);
+			} while (rs.next());
+
+			db.close();
+
+			return products;
+		} catch (SQLException ex) {
+			Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return null;
+	}
 }
