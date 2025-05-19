8e13c6294b76be284c12c952314f8bcf8315aef8
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-05-19 09:15:48 +0700

    feat: only allow numbers for fee input

--- src/main/java/pages/cashier/Cashier_Transaction.java
+++ src/main/java/pages/cashier/Cashier_Transaction.java
@@ -37,6 +37,10 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
 
 public class Cashier_Transaction {
 	private static BiConsumer<Content_Panel, Integer> reloadCallback;
@@ -212,6 +216,21 @@ public class Cashier_Transaction {
         JLabel serviceLabel = new JLabel("Jasa:");
         serviceLabel.setFont(new Font("Arial", Font.BOLD, 16));
         JTextField serviceField = new JTextField();
+        ((AbstractDocument) serviceField.getDocument()).setDocumentFilter(new DocumentFilter() {
+            @Override
+            public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+                if (string.matches("\\d+")) {
+                    super.insertString(fb, offset, string, attr);
+                }
+            }
+
+            @Override
+            public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+                if (text.matches("\\d+")) {
+                    super.replace(fb, offset, length, text, attrs);
+                }
+            }
+        });
         Cashier_Transaction.serviceField = serviceField;
         serviceField.setPreferredSize(new Dimension(300, 50));
         serviceField.setText("0");
